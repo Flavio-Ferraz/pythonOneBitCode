b913b52b4683adf2936abb3375109d30adc355e7
Flavio Ferraz <flavioferraz80@gmail.com>
2024-11-08 17:25:16 -0300

    Atualização - aula 17 - Fundamentos

---
+++ 17-opAritmeticos.py
@@ -0,0 +1,41 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+# aritméticos 
+sum = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mult = num1 * num2
+
+mod = num1 % num2
+expo = num1 ** num2
+
+print(sum)
+print(sub)
+print(div)
+print(mult)
+print(f"O resto da divisão de {num1} por {num2} é {mod}")
+print(expo)
+
+#operador de comparação - retornarão valores booleanos
+bigger = num1 > num2
+smaller = num1 < num2
+equal = num1 == num2
+different = num1 != num2
+bigger_equal = num1 >= num2
+smaller_equal = num1 <= num2
+
+print(bigger)
+print(smaller)
+print(equal)
+print(different)
+print(smaller_equal)
+print(bigger_equal)
+
+# Operadores de atribuição
+num1 = num1 + 1 
+num1 += 1 # num1 = num1 + 1
+num1 -= 1 # num1 = num1 - 1
+num1 *= 1 # num1 = num1 * 1
+num1 /= 1 # num1 = num1 / 1
+
