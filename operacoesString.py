43d67924b30f4de89fd7a4bab5fe7b7149fdb390
Flavio Ferraz <flavioferraz80@gmail.com>
2025-01-29 21:01:08 -0300

    Atualização do curso

---
+++ operacoesString.py
@@ -0,0 +1,17 @@
+gameName = "FIFA"
+gameVersion = " 24"
+
+gameFullName = gameName + gameVersion #concatenação de stings
+line ="="
+print(line * 25)
+print(gameFullName)
+
+#Multiplicação de caracteres dentro de Strings
+line ="="
+print(line * 25)
+
+#Procurar uma string dentro de um texto
+gameDescription = '''Fifa 23 é um jogo de futebol 
+desenvolvido pela EA Sports
+que possibilita jogar localmente ou online'''
+print("Fifa" in gameDescription) #"Existe a palavra Fifa dentro da string armazenada em gameDescription?" o retorno será um valor booleano.
